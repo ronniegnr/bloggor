c163a32ff7f8b37922cad57d9a6b4739fc5fe32d
ronniegnr <gnr.ronnie@gmail.com>
2022-09-08 14:59:36 +0600

    papper project initiated

---
+++ src/test/java/bd/com/ronnie/papper/PapperApplicationTests.java
@@ -0,0 +1,13 @@
+package bd.com.ronnie.papper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
